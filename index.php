44424460e5adc4f5e7e737575e1469d0e0356427
Erlan433 <erlanusmonov47@gmail.com>
2026-06-26 17:28:00 +0300

    bag

--- index.php
+++ index.php
@@ -44,8 +44,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DPR &mdash; продажа пустых помещений</title>
     <link rel="icon" href="/images/dpr-logo.jpg" type="image/x-icon">
-    <link rel="stylesheet" href="/css/style.css?m=<?php echo rand() ?>">
     <link rel="stylesheet" href="/css/main.css?m=<?php echo rand() ?>">
+    <link rel="stylesheet" href="/css/home/style.css?m=<?php echo rand() ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="/fontawesome/css/all.css" rel="stylesheet">
